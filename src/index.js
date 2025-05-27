cbeeb6cfbe62a18c5ac7ba3bb98309e167a09310
julianval20 <jevalenciav@udistrital.edu.co>
2025-05-27 07:33:40 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from "react-intl";
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+
+
+const language = navigator.language || navigator.userLanguage;
+
+
+let locale = "en";
+let messages = localeEnMessages;
+
+if (language.startsWith("es")) {
+  locale = "es";
+  messages = localeEsMessages;
+}
+
+ReactDOM.render(
+  <IntlProvider locale={locale} messages={messages}>
+    <JobsList />
+  </IntlProvider>,
+  document.getElementById("root")
+);
